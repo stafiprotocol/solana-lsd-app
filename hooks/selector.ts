4a09ae707bcfc94def9a304109ea0df1abc9a0a1
Jeff L <jeffliu2857@gmail.com>
2025-05-07 17:00:56 +0800

    refine useAppSlice hook

--- hooks/selector.ts
+++ hooks/selector.ts
@@ -1,15 +1,8 @@
-import { RootState } from "redux/store";
 import { useAppSelector } from "./common";
 
 export function useAppSlice() {
   const { darkMode, updateFlag, unreadNoticeFlag } = useAppSelector(
-    (state: RootState) => {
-      return {
-        darkMode: state.app.darkMode,
-        updateFlag: state.app.updateFlag,
-        unreadNoticeFlag: state.app.unreadNoticeFlag,
-      };
-    }
+    (state) => state.app
   );
 
   return { darkMode, updateFlag, unreadNoticeFlag };
